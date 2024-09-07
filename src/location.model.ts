aaf1f37f84af4106b4f059f78bb58006533b0ad3
RafaRem <edgarembao@hotmail.com>
2024-09-06 19:46:00 -0700

    ajuste de model location

--- src/location.model.ts
+++ src/location.model.ts
@@ -1,9 +1,11 @@
 import { EntityModel } from "anttojo-models";
 
 export interface LocationModel extends EntityModel{
-    latitud?: string;
-    longitud?: string;
-    street?: string;
-    outerNumber?: string;
-    interiorNumber?: string;
+    address?: string;
+    country?: string;
+    state?: string;
+    city ?: string;
+    cp?: string;
+    lat?: string;
+    long?: string;
 }
