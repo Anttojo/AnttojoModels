498958a55ca1b0cf6efbecf2a522aebdc833aceb
RafaRem <edgarembao@hotmail.com>
2024-09-17 18:28:25 -0700

    ajsute muchos tipos

--- src/product.model.ts
+++ src/product.model.ts
@@ -1,14 +1,14 @@
-import { CategoryModel } from "./category.model";
-import { EntityModel } from "./entity.model";
-import { SubCategoryModel } from "./subcategory.model";
-import { TypeProductModel } from "./type-product.model";
+import { CategoryModel } from './category.model';
+import { EntityModel } from './entity.model';
+import { SubCategoryModel } from './subcategory.model';
+import { TypeProductModel } from './type-product.model';
 
 export interface ProductModel extends EntityModel {
   name: string;
   description: string;
   price: number;
   images?: string[];
-  type: TypeProductModel;
+  type: TypeProductModel[];
   categories: CategoryModel[];
   subcategories: SubCategoryModel[];
 
